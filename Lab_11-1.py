c329b6a3d4a54e3a386c8fe362a8aea79df35c31
p23pylons <p23plyons@fairfieldprep.org>
2023-01-30 08:49:17 -0500

    Submission of Cycle 11 Day 1 Lab

---
+++ Lab_11-1.py
@@ -0,0 +1,12 @@
+# Author: PCL 1/30/23
+
+#defining a dictionary of students in the class to their computer number
+dictionary_1 = {"Sean":7, "Jan": 3, "Preston":0}
+
+print(dictionary_1)
+
+#5. 
+dictionary_1["Someone Else"] = dictionary_1["Preston"]
+del dictionary_1["Preston"]
+
+print(dictionary_1)
