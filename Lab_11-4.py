d6f8a02c665ad15aec7913ab50912cccd171db68
p23pylons <p23plyons@fairfieldprep.org>
2023-02-02 12:36:55 -0500

    Submission of Cycle 11 Day 4 Labs

---
+++ Lab_11-4.py
@@ -0,0 +1,9 @@
+# Author: PCL 2/2/23
+
+weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+weekends = ("Saturday", "Sunday")
+
+days_of_the_week = weekdays+weekends
+
+print(days_of_the_week)
---
+++ Lab_11-5.py
@@ -0,0 +1,6 @@
+# Author: PCL 2/2/23
+
+day_date = (2, 3, 4, 5, 6)
+(thu,fri,sat,sun,mon) = day_date
+
+print(thu,fri,sat,sun,mon)
